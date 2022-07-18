8afedfe2d354cc211cfe0482928cd58cf7a937de
John Seong <39040639+sandole@users.noreply.github.com>
2022-07-18 15:16:45 -0600

    deploy wordpress application

---
+++ deploy-app.ps1
@@ -0,0 +1,37 @@
+# App location variable
+$Dir = "$Home\Desktop\AppLocation\m04"
+
+# ARO Cluster
+$env:KUBECONFIG="kubeconfig_aro"
+
+# Deploy nginx
+./oc create deployment nginx --image=nginx
+
+# Delete nginx
+./oc delete deployment nginx
+
+# Deploy in code
+code $Dir\nginx.yaml
+./oc apply -f $Dir\nginx.yaml
+./oc get deployment,pods
+./oc expose deployment nginx --port=80 --target-port=80 --type=LoadBalancer
+
+$SERVICEIP=(./oc get service nginx -o jsonpath='{ .status.loadBalancer.ingress[0].ip }')
+Start-Process http://$SERVICEIP
+
+# OCP Cluster context
+$env:KUBECONFIG="kubeconfig_ocp"
+./oc apply -f $Dir\nginx.yaml
+
+# Use node port service
+./oc expose deployment nginx --target-port=80 --type=NodePort
+
+# Save port
+$PORT = (./oc get service nginx -o jsonpath='{.spec.ports[0].nodePort }')
+
+# Start nodes on this port
+Start-Process http://ocp-w-2.ocp.mycustomdomain.io:$PORT
+Start-Process http://ocp-w-3.ocp.mycustomdomain.io:$PORT
+
+# Example WordPress application
+./oc new-project wordpress
